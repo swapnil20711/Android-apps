017fd81bfd6d3855b06177f506a65b33bf37887a
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-04-11 18:27:46 +0530

    Intents and activity names added

--- Miwok/app/src/main/java/com/swapnil/miwok/MainActivity.java
+++ Miwok/app/src/main/java/com/swapnil/miwok/MainActivity.java
@@ -1,7 +1,10 @@
 package com.swapnil.miwok;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openNumbersList(View view) {
+        Intent Numbers = new Intent(this, NumbersActivity.class);
+        startActivity(Numbers);
+
+
+    }
+
+    public void openFamilyList(View view) {
+        Intent Family = new Intent(this, FamilyActivity.class);
+        startActivity(Family);
+    }
+
+    public void openColorsList(View view) {
+        Intent Color = new Intent(this, ColorsActivity.class);
+        startActivity(Color);
+    }
+
+    public void openPhrasesList(View view) {
+        Intent Phrase = new Intent(this, PhrasesActivity.class);
+        startActivity(Phrase);
     }
 }
--- Miwok/app/src/main/java/com/swapnil/miwok/NumbersActivity.java
+++ Miwok/app/src/main/java/com/swapnil/miwok/NumbersActivity.java
@@ -9,6 +9,6 @@ public class NumbersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_numbers);
     }
 }
