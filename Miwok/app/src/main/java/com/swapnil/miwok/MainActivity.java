8a690a40e7078be8cb374c38a9237df4215723b2
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-04-14 17:58:40 +0530

    OnclickListeners added

--- Miwok/app/src/main/java/com/swapnil/miwok/MainActivity.java
+++ Miwok/app/src/main/java/com/swapnil/miwok/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView num=findViewById(R.id.numbers);
     }
+
 
     public void openNumbersList(View view) {
         Intent Numbers = new Intent(this, NumbersActivity.class);
