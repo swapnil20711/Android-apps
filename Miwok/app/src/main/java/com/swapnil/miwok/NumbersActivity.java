97e04e6de535425194350d3a5f800e9603166a12
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-04-18 21:41:03 +0530

    Numbers array created

--- Miwok/app/src/main/java/com/swapnil/miwok/NumbersActivity.java
+++ Miwok/app/src/main/java/com/swapnil/miwok/NumbersActivity.java
@@ -3,12 +3,17 @@ package com.swapnil.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
+    private String []words=new String[]{"one","two","three","four","five","six","seven","eight","nine","ten"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+        for(int i=0;i<words.length;i++) {
+            Log.v("NumbersActivity", words[i]);
+        }
     }
 }
