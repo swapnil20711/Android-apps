74d0211a78b2cf2d285f69132a290de20cabae5e
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-04-23 20:34:40 +0530

    Added arraylist

--- Miwok/app/src/main/java/com/swapnil/miwok/NumbersActivity.java
+++ Miwok/app/src/main/java/com/swapnil/miwok/NumbersActivity.java
@@ -5,15 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class NumbersActivity extends AppCompatActivity {
-    private String []words=new String[]{"one","two","three","four","five","six","seven","eight","nine","ten"};
+
+    private ArrayList<String> words = new ArrayList<>(Arrays.asList("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
-        for(int i=0;i<words.length;i++) {
-            Log.v("NumbersActivity", words[i]);
+        for (int i = 0; i < words.size(); i++) {
+            Log.v("NumbersActivity", words.get(i));
         }
     }
 }
